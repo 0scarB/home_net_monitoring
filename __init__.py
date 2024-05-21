3308e19d0c95bebd0c45c4e4123b115e7ef77904
0scarB <oscarb@protonmail.com>
2024-05-22 00:13:37 +0200

    Append to checks.txt file

--- __init__.py
+++ __init__.py
@@ -1,20 +1,34 @@
+import math
+import random
+import time
+
 STATE_COUNTING_NUMBER_OF_CHECKS = 0
 STATE_RUNNING_CHECKS            = 1
+SUBCHECK_TYPE_REQUEST_URL = "request_url"
+STATUS_SUCCEEDED = "succeeded"
+STATUS_FAILED    = "failed"
+INDENT = "\t"
 
-state         = -1
-checks_file   = "./checks.txt"
-checks_n      = 0
-current_check = 0
+state              = -1
+checks_file_path   = "./checks.txt"
+checks_file_handle = None
+checks_n           = 0
+current_check      = 0
 
 
 def run_checks(func):
-    global state
+    global state, checks_file_handle
 
     state = STATE_COUNTING_NUMBER_OF_CHECKS
     func()
     state = STATE_RUNNING_CHECKS
-    while current_check < checks_n:
-        func()
+    with open(checks_file_path, "a") as checks_file_handle:
+        while current_check < checks_n:
+            timestamp = math.floor(time.time())
+            if checks_file_handle == None: raise Exception
+            checks_file_handle.write(f"check_begin_at_timestamp {timestamp}\n")
+            func()
+            checks_file_handle.write(f"check_end\n")
 
 
 def next_check():
@@ -33,4 +47,11 @@ def next_check():
 def request_url(url):
     print(f"Making request to '{url}'.")
     print("Not implemented!")
+    response_time = random.randrange(1000)/1000
+    if checks_file_handle == None: raise Exception
+    checks_file_handle.write(f"{INDENT}subcheck_begin {SUBCHECK_TYPE_REQUEST_URL}\n")
+    checks_file_handle.write(f"{INDENT}{INDENT}status {STATUS_SUCCEEDED}\n")
+    checks_file_handle.write(f"{INDENT}{INDENT}url {url}\n")
+    checks_file_handle.write(f"{INDENT}{INDENT}response_time_in_secs {response_time}\n")
+    checks_file_handle.write(f"{INDENT}subcheck_end\n")
 
