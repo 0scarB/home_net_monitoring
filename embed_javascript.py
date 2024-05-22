21af844cf87b892d8f2fcd958e79b029d4b3b56a
0scarB <oscarb@protonmail.com>
2024-05-22 09:37:14 +0200

    Enable hot reloading in dev mode

--- embed_javascript.py
+++ embed_javascript.py
@@ -1,18 +1,27 @@
 #!/usr/bin/env python3
 
-embedding = ""
-hex_digits = "0123456789ABCDEF"
-with open("index.js", "rb") as f:
-    for byte in f.read():
-        embedding += "\\x" + hex_digits[byte>>4] + hex_digits[byte&0xF]
 
-new_content = ""
-with open("__init__.py", "r") as f:
-    for line in f.readlines():
-        if line.startswith('EMBEDDED_JAVASCRIPT_FILE_BYTES = b"'):
-            new_content += 'EMBEDDED_JAVASCRIPT_FILE_BYTES = b"' + embedding + '"'
-        else:
-            new_content += line
+def embed_javascript():
+    file_dir = "/".join(__file__.split("/")[:-1])
 
-with open("__init__.py", "w") as f:
-    f.write(new_content)
+    embedding = ""
+    hex_digits = "0123456789ABCDEF"
+    with open(file_dir + "/index.js", "rb") as f:
+        for byte in f.read():
+            embedding += "\\x" + hex_digits[byte>>4] + hex_digits[byte&0xF]
+
+    new_content = ""
+    with open(file_dir + "/__init__.py", "r") as f:
+        for line in f.readlines():
+            if line.startswith('EMBEDDED_JAVASCRIPT_FILE_BYTES = b"'):
+                new_content += 'EMBEDDED_JAVASCRIPT_FILE_BYTES = b"' + embedding + '"'
+            else:
+                new_content += line
+
+    with open(file_dir + "/__init__.py", "w") as f:
+        f.write(new_content)
+
+
+if __name__ == "__main__":
+    embed_javascript()
+
