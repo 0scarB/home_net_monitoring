93f5176354ecfa2a0bef923b7adff6a1bf81e06f
0scarB <oscarb@protonmail.com>
2024-05-22 04:09:38 +0200

    Send/receive /checks requests and aggregate results

--- __init__.py
+++ __init__.py
@@ -8,13 +8,13 @@ COMMAND_SERVE_MONITORING_PAGE = "serve"
 STATE_COUNTING_NUMBER_OF_CHECKS = 0
 STATE_RUNNING_CHECKS            = 1
 STATE_SERVING_MONITORING_PAGE   = 2
-SUBCHECK_TYPE_REQUEST_URL = "request_url"
+CHECK_TYPE_REQUEST_URL = "request_url"
 STATUS_SUCCEEDED = "succeeded"
 STATUS_FAILED    = "failed"
 
 _command             = ""
 state                = -1
-checks_file_path     = "./checks.jsonld"
+checks_file_path     = "./checks.jsonl"
 checks_file_handle   = None
 checks_n             = 0
 current_check        = 0
@@ -38,7 +38,7 @@ def run_monitor(func):
             while current_check < checks_n:
                 timestamp = math.floor(time.time())
                 if checks_file_handle == None: raise Exception
-                checks_file_handle.write('{"timestamp": ' + str(timestamp) + ', checks: [')
+                checks_file_handle.write('{"timestamp": ' + str(timestamp) + ', "checks": [')
                 func()
                 checks_file_handle.write("]}\n")
         return
@@ -68,11 +68,11 @@ def request_url(url):
     print("Not implemented!")
     response_time = random.randrange(1000)/1000
     if checks_file_handle == None: raise Exception
-    checks_file_handle.write('{"type": "'  + SUBCHECK_TYPE_REQUEST_URL + '", ')
+    checks_file_handle.write('{"type": "'  + CHECK_TYPE_REQUEST_URL + '", ')
     checks_file_handle.write('"status": "' + STATUS_SUCCEEDED          + '", ')
     checks_file_handle.write('"url": "'    + url                       + '", ')
-    checks_file_handle.write('"response_time_in_secs": "'
-                                           + str(response_time)        + '"}')
+    checks_file_handle.write('"response_time_in_secs": '
+                                           + str(response_time)        + '}'  )
     if current_check < checks_n:
         checks_file_handle.write(", ")
 
@@ -90,11 +90,79 @@ def serve_monitoring_page():
                 self.end_headers()
                 self.wfile.write(bytes(response_content, "utf-8"))
             elif self.path == "/index.js":
+                # Uncomment for "dev"
+                with open(__file__[:-len("__init__.py")] + "index.js") as f:
+                    response_content = f.read()
                 self.send_response(200)
                 self.send_header("Content-Type", "application/javascript; charset=utf-8")
-                self.send_header("Content-Length", str(len(EMBEDDED_JAVASCRIPT_FILE_BYTES)))
+                self.send_header("Content-Length", str(len(response_content)))
                 self.end_headers()
-                self.wfile.write(EMBEDDED_JAVASCRIPT_FILE_BYTES)
+                self.wfile.write(bytes(response_content, "utf-8"))
+
+                # Uncomment for "prod"
+                #self.send_response(200)
+                #self.send_header("Content-Type", "application/javascript; charset=utf-8")
+                #self.send_header("Content-Length", str(len(EMBEDDED_JAVASCRIPT_FILE_BYTES)))
+                #self.end_headers()
+                #self.wfile.write(EMBEDDED_JAVASCRIPT_FILE_BYTES)
+            elif self.path.startswith("/checks"):
+                # Attempt to parse start and end timestamps in query string
+                start_timestamp = -1
+                end_timestamp   = -1
+                try:
+                    i = len("/checks")
+                    if self.path[i] != '?':
+                        raise
+                    i += 1
+                    if self.path[i:i + 6] != "start=":
+                        raise
+                    i += 6
+                    timestamp_start_i = i
+                    while self.path[i].isdigit():
+                        i += 1
+                    start_timestamp = int(self.path[timestamp_start_i:i])
+                    if self.path[i] != '&':
+                        raise
+                    i += 1
+                    if self.path[i:i + 4] != "end=":
+                        raise
+                    i += 4
+                    timestamp_start_i = i
+                    while i < len(self.path) and self.path[i].isdigit():
+                        i += 1
+                    end_timestamp = int(self.path[timestamp_start_i:i])
+                # Reponse with 400 on failure
+                except Exception:
+                    response_content = "Invalid /checks request!"
+                    self.send_response(400)
+                    self.send_header("Content-Type", "text/plain; charset=utf-8")
+                    self.send_header("Content-Length", str(len(response_content)))
+                    self.end_headers()
+                    self.wfile.write(bytes(response_content, "utf-8"))
+                # Respond with JSON on success
+                else:
+                    timestamp_line_offset = len('{"timestamp": ')
+                    response_content = "["
+                    with open(checks_file_path) as f:
+                        first_item = True
+                        for line in f.readlines():
+                            i = timestamp_start_i = timestamp_line_offset
+                            while i < len(line) and line[i].isdigit():
+                                i += 1
+                            timestamp = int(line[timestamp_start_i:i])
+                            if start_timestamp <= timestamp <= end_timestamp:
+                                if first_item:
+                                    first_item = False
+                                else:
+                                    response_content += ", "
+                                response_content += line[:-1]
+                    response_content += "]"
+
+                    self.send_response(200)
+                    self.send_header("Content-Type", "application/json; charset=utf-8")
+                    self.send_header("Content-Length", str(len(response_content)))
+                    self.end_headers()
+                    self.wfile.write(bytes(response_content, "utf-8"))
             else:
                 response_content = "File not found!"
                 self.send_response(404)
@@ -178,4 +246,4 @@ def build_monitoring_page_html():
     return html
 
 
-EMBEDDED_JAVASCRIPT_FILE_BYTES = b"\x63\x6F\x6E\x73\x6F\x6C\x65\x2E\x6C\x6F\x67\x28\x22\x48\x65\x6C\x6C\x6F\x2C\x20\x57\x6F\x72\x6C\x64\x21\x22\x29\x3B\x0A"
+EMBEDDED_JAVASCRIPT_FILE_BYTES = b"\x61\x73\x79\x6E\x63\x20\x66\x75\x6E\x63\x74\x69\x6F\x6E\x20\x6D\x61\x69\x6E\x28\x29\x20\x7B\x0A\x20\x20\x20\x20\x72\x65\x73\x20\x3D\x20\x61\x77\x61\x69\x74\x20\x66\x65\x74\x63\x68\x28\x60\x2F\x63\x68\x65\x63\x6B\x73\x3F\x73\x74\x61\x72\x74\x3D\x30\x26\x65\x6E\x64\x3D\x24\x7B\x4D\x61\x74\x68\x2E\x63\x65\x69\x6C\x28\x44\x61\x74\x65\x2E\x6E\x6F\x77\x28\x29\x2F\x31\x30\x30\x30\x29\x7D\x60\x29\x0A\x20\x20\x20\x20\x63\x68\x65\x63\x6B\x73\x20\x3D\x20\x72\x65\x73\x2E\x6A\x73\x6F\x6E\x28\x29\x0A\x20\x20\x20\x20\x63\x6F\x6E\x73\x6F\x6C\x65\x2E\x6C\x6F\x67\x28\x63\x68\x65\x63\x6B\x73\x29\x0A\x7D\x0A\x0A\x6D\x61\x69\x6E\x28\x29\x0A\x0A"
--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log("Hello, World!");
+const CHECK_TYPE_REQUEST_URL = "request_url";
+
+async function main() {
+    const res   = await fetch(`/checks?start=0&end=${Math.ceil(Date.now()/1000)}`);
+    const items = await res.json();
+
+    const aggregate = {};
+    for (const item of items) {
+        const t = item.timestamp;
+        for (const check of item.checks) {
+            let id, y;
+            switch (check.type) {
+                case CHECK_TYPE_REQUEST_URL:
+                    id = `${check.url} -- Requests`;
+                    y  = check.response_time_in_secs;
+                    break;
+            }
+            if (typeof aggregate[id] === "undefined") {
+                aggregate[id] = {
+                    ts: [t],
+                    ys: [y],
+                    most_recent_t: t,
+                    most_recent_status: check.status,
+                };
+            } else {
+                aggregate[id].ts.push(t);
+                aggregate[id].ys.push(y);
+                aggregate[id].most_recent_t      = t;
+                aggregate[id].most_recent_status = check.status;
+            }
+        }
+    }
+    console.log(aggregate);
+}
+
+main();
+
