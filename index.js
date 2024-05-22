79eaebd9e4767b2ab9a412f44bdbf6bf319da76e
0scarB <oscarb@protonmail.com>
2024-05-22 11:04:31 +0200

    Display timeseries values

--- index.js
+++ index.js
@@ -1,36 +1,105 @@
 const CHECK_TYPE_REQUEST_URL = "request_url";
+const STATUS_SUCCEEDED = "succeeded";
+const STATUS_FAILED    = "failed";
+
+let currentTimestampInSecs;
 
 async function main() {
-    const res   = await fetch(`/checks?start=0&end=${Math.ceil(Date.now()/1000)}`);
+    currentTimestampInSecs = Math.ceil(Date.now()/1000);
+    const res   = await fetch(`/checks?start=0&end=${currentTimestampInSecs}`);
     const items = await res.json();
 
-    const aggregate = {};
+    const aggregates = {};
     for (const item of items) {
         const t = item.timestamp;
         for (const check of item.checks) {
-            let id, y;
+            let id, y, status;
             switch (check.type) {
                 case CHECK_TYPE_REQUEST_URL:
                     id = `${check.url} -- Requests`;
                     y  = check.response_time_in_secs;
                     break;
             }
-            if (typeof aggregate[id] === "undefined") {
-                aggregate[id] = {
+            if (typeof aggregates[id] === "undefined") {
+                aggregates[id] = {
                     ts: [t],
                     ys: [y],
-                    most_recent_t: t,
-                    most_recent_status: check.status,
+                    statuses: [check.status],
                 };
             } else {
-                aggregate[id].ts.push(t);
-                aggregate[id].ys.push(y);
-                aggregate[id].most_recent_t      = t;
-                aggregate[id].most_recent_status = check.status;
+                aggregates[id].ts.push(t);
+                aggregates[id].ys.push(y);
+                aggregates[id].statuses.push(check.status);
             }
         }
     }
-    console.log(aggregate);
+
+    const mainEl = document.getElementsByTagName("main")[0];
+    for (const [id, aggregate] of Object.entries(aggregates)) {
+        const el = document.createElement("div");
+        el.appendChild(document.createTextNode(id));
+        const lastStatus = aggregate.statuses[aggregate.statuses.length - 1];
+        el.appendChild(document.createTextNode(
+            ` -- Last ${lastStatus}`));
+        el.appendChild(document.createElement("br"));
+
+        const canvasEl = document.createElement("canvas");
+        canvasEl.style.width  = `${mainEl.clientWidth}px`;
+        canvasEl.style.height = "64px";
+        el.appendChild(canvasEl);
+
+        el.appendChild(document.createElement("br"));
+        el.appendChild(document.createElement("br"));
+
+        mainEl.appendChild(el);
+
+        renderTimeSeriesCanvas(aggregate, canvasEl);
+    }
+}
+
+function renderTimeSeriesCanvas({ts, ys, statuses}, canvasEl) {
+    canvasEl.width = canvasEl.clientWidth;
+    canvasEl.height = canvasEl.clientHeight;
+
+    let tMin = ts[0];
+    let tMax = currentTimestampInSecs;
+    let tMinMaxDiff = tMax - tMin;
+    tMin -= 0.1*tMinMaxDiff;
+    tMax += 0.1*tMinMaxDiff;
+    const pxPerSec = canvasEl.width/(tMax - tMin);
+    const maxY = Math.max(...ys);
+
+    const ctx = canvasEl.getContext("2d");
+
+    // Fill background
+    ctx.fillStyle = "#222";
+    ctx.fillRect(0, 0, canvasEl.width, canvasEl.height);
+
+    // Draw "pins" for timeseries values
+    for (let i = 0; i < ts.length; ++i) {
+        const t = ts[i];
+        const y = ys[i];
+        const status = statuses[i];
+
+        const pinTopRadius = 4;
+        const pinX         = (t - tMin)*pxPerSec;
+        const pinTop       =
+            ((maxY - y)/maxY)*(canvasEl.height - 2*pinTopRadius) + pinTopRadius;
+        const pinBottom    = canvasEl.height;
+        const pinColor     = status === STATUS_FAILED ? "#F00" : "#0FF";
+
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = pinColor;
+        ctx.beginPath();
+        ctx.moveTo(pinX, pinBottom);
+        ctx.lineTo(pinX, pinTop);
+        ctx.stroke();
+
+        ctx.fillStyle = pinColor;
+        ctx.beginPath();
+        ctx.arc(pinX, pinTop, pinTopRadius, 0, 360, 0);
+        ctx.fill();
+    }
 }
 
 main();
